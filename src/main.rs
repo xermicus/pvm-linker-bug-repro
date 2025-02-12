d13ceb68fc02f3d01dd02e9c19c55cb86dadc4d8
xermicus <cyrill@parity.io>
2025-02-12 13:24:06 +0100

    update
    Signed-off-by: xermicus <cyrill@parity.io>

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,11 @@
 use polkavm_linker::{program_from_elf, Config};
 use revive_runner::*;
 
-const CALLEE_HASH: [u8; 32] = [
-    18, 33, 95, 162, 209, 246, 157, 38, 237, 5, 183, 222, 86, 105, 100, 222, 148, 9, 244, 196, 163,
-    60, 180, 193, 130, 168, 26, 231, 109, 14, 110, 203,
-];
-
-fn main() {
-    let elf = include_bytes!("buggy.so");
+// Is expected to return 0.
+fn mload() {
+    let elf = include_bytes!("mload.so");
     let mut config = Config::default();
-    config.set_optimize(true); // doesn't repro if `false`
+    config.set_optimize(true);
     let blob = program_from_elf(config, elf).unwrap();
 
     // reproduce the bug
@@ -17,26 +13,25 @@ fn main() {
         actions: vec![
             SpecsAction::Instantiate {
                 origin: TestAddress::Alice,
-                value: 100_000,
+                value: 0,
                 gas_limit: None,
                 storage_deposit_limit: Some(10_000_000),
                 code: Code::Bytes(blob),
-                data: vec![],
+                data: vec![1; 20],
                 salt: Default::default(),
-            },
-            SpecsAction::Upload {
-                origin: TestAddress::Alice,
-                code: Code::Bytes(include_bytes!("return_with_data.polkavm").to_vec()),
-                storage_deposit_limit: Some(10_000_000),
             },
             SpecsAction::Call {
                 origin: TestAddress::Alice,
                 dest: TestAddress::Instantiated(0),
-                value: 100_000,
+                value: 0,
                 gas_limit: None,
                 storage_deposit_limit: Some(10_000_000),
-                data: CALLEE_HASH.to_vec(),
+                data: vec![0xe2, 0x17, 0x9b, 0x8e],
             },
+            SpecsAction::VerifyCall(VerifyCallExpectation {
+                output: vec![0; 32].into(),
+                ..Default::default()
+            }),
         ],
         ..Default::default()
     };
@@ -44,4 +39,38 @@ fn main() {
     let result = specs.run();
 
     dbg!(result);
+}
+
+// Traps with `OutOfBounds`, is expected to succeed (or another error but not OOB).
+fn erc() {
+    let elf = include_bytes!("erc.so");
+    let mut config = Config::default();
+    config.set_optimize(true);
+    let blob = program_from_elf(config, elf).unwrap();
+
+    // reproduce the bug
+    let specs = Specs {
+        actions: vec![SpecsAction::Instantiate {
+            origin: TestAddress::Alice,
+            value: 0,
+            gas_limit: None,
+            storage_deposit_limit: Some(100_000_000),
+            code: Code::Bytes(blob),
+            data: vec![
+                00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 11, 11, 11, 11, 11, 11, 11, 11, 11,
+                11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11,
+            ],
+            salt: Default::default(),
+        }],
+        ..Default::default()
+    };
+
+    let result = specs.run();
+
+    dbg!(result);
+}
+
+fn main() {
+    erc();
+    mload();
 }
